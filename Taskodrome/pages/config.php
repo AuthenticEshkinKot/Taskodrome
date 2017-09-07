e39c45107c54886f43c1850611f3b7200bab3275
max <maxrider@mail.ru>
2017-09-07 23:21:14 +0300

    Unique configuration for each project

--- Taskodrome/pages/config.php
+++ Taskodrome/pages/config.php
@@ -7,26 +7,26 @@ access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
 
 $f_status_order = gpc_get_string( 'status_board_order' );
 
-if( plugin_config_get( 'status_board_order' ) != $f_status_order ) {
-  plugin_config_set( 'status_board_order', explode(';', $f_status_order) );
+if( plugin_config_get( 'status_board_order', null, false, null, helper_get_current_project() ) != $f_status_order ) {
+  plugin_config_set( 'status_board_order', explode(';', $f_status_order), NO_USER, helper_get_current_project() );
 }
 
 $f_cooldown_period_days = gpc_get_int( 'cooldown_period_days' );
 
-if( plugin_config_get( 'cooldown_period_days' ) != $f_cooldown_period_days ) {
-  plugin_config_set( 'cooldown_period_days', $f_cooldown_period_days );
+if( plugin_config_get( 'cooldown_period_days', null, false, null, helper_get_current_project() ) != $f_cooldown_period_days ) {
+  plugin_config_set( 'cooldown_period_days', $f_cooldown_period_days, NO_USER, helper_get_current_project() );
 }
 
 $f_cooldown_period_hours = gpc_get_int( 'cooldown_period_hours' );
 
-if( plugin_config_get( 'cooldown_period_hours' ) != $f_cooldown_period_hours ) {
-  plugin_config_set( 'cooldown_period_hours', $f_cooldown_period_hours );
+if( plugin_config_get( 'cooldown_period_hours', null, false, null, helper_get_current_project() ) != $f_cooldown_period_hours ) {
+  plugin_config_set( 'cooldown_period_hours', $f_cooldown_period_hours, NO_USER, helper_get_current_project() );
 }
 
 $f_hidden_users = gpc_get_string( 'hidden_users' );
 
-if( plugin_config_get( 'hidden_users' ) != $f_hidden_users ) {
-  plugin_config_set( 'hidden_users', explode(';', $f_hidden_users) );
+if( plugin_config_get( 'hidden_users', null, false, null, helper_get_current_project() ) != $f_hidden_users ) {
+  plugin_config_set( 'hidden_users', explode(';', $f_hidden_users), NO_USER, helper_get_current_project()  );
 }
 
 form_security_purge( 'plugin_format_config_edit' );
