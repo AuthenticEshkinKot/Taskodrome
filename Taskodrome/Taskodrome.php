f264dd13000a24ee7a5f97c25f13ad14a07d2f18
max <maxrider@mail.ru>
2018-02-23 15:21:32 +0300

    Version increased

--- Taskodrome/Taskodrome.php
+++ Taskodrome/Taskodrome.php
@@ -11,7 +11,7 @@ class TaskodromePlugin extends MantisPlugin
     $this->description = plugin_lang_get("description");
     $this->page = 'config_page';
 
-    $this->version = "1.4.0";
+    $this->version = "1.4.1";
     $this->requires = array(
       "MantisCore" => "2.0.0",
     );
